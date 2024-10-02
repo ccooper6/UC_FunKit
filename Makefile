c9761173273ccb6969128605aa516ba5c1bcd38b
cco139 <cco139@uclive.ac.nz>
2024-10-02 14:02:41 +1300

    Created README, added the bar slider to the led matrix with position checks

--- Makefile
+++ Makefile
@@ -16,7 +16,7 @@ all: game.out
 
 
 # Compile: create object files from C source files.
-game.o: game.c ../../drivers/avr/system.h ../../drivers/display.h ../../fonts/font5x7_1.h ../../utils/font.h ../../utils/pacer.h ../../utils/tinygl.h
+game.o: game.c ../../drivers/avr/system.h ../../drivers/display.h ../../fonts/font5x7_1.h ../../utils/font.h ../../utils/pacer.h ../../utils/tinygl.h ../../drivers/navswitch.h
 	$(CC) -c $(CFLAGS) $< -o $@
 
 pio.o: ../../drivers/avr/pio.c ../../drivers/avr/pio.h ../../drivers/avr/system.h
@@ -43,10 +43,13 @@ pacer.o: ../../utils/pacer.c ../../drivers/avr/system.h ../../drivers/avr/timer.
 tinygl.o: ../../utils/tinygl.c ../../drivers/avr/system.h ../../drivers/display.h ../../utils/font.h ../../utils/tinygl.h
 	$(CC) -c $(CFLAGS) $< -o $@
 
+navswitch.o: ../../drivers/navswitch.c ../../drivers/avr/delay.h ../../drivers/avr/pio.h ../../drivers/avr/system.h ../../drivers/navswitch.h
+	$(CC) -c $(CFLAGS) $< -o $@
+
 
 
 # Link: create ELF output file from object files.
-game.out: game.o pio.o system.o timer.o display.o ledmat.o font.o pacer.o tinygl.o
+game.out: game.o pio.o system.o timer.o display.o ledmat.o font.o pacer.o tinygl.o navswitch.o
 	$(CC) $(CFLAGS) $^ -o $@ -lm
 	$(SIZE) $@
 
--- game.c
+++ game.c
@@ -2,31 +2,62 @@
 #include "pacer.h"
 #include "tinygl.h"
 #include "../fonts/font5x7_1.h"
-
+#include "navswitch.h"
 
 #define PACER_RATE 500
 #define MESSAGE_RATE 10
+#define X_POS 4
 
-void showStartingMessage(void) {
-    tinygl_font_set (&font5x7_1);
-    tinygl_text_speed_set (MESSAGE_RATE);
-    tinygl_text("Group 306 - PONG!");
-    tinygl_text_mode_set (TINYGL_TEXT_MODE_SCROLL);
-}
+//static int update_count = 0;
+//static int total_updates = 0;
+
+//void showStartingMessage(void) {
+//    const char* msg = "Group 306 - PONG!";
+//    tinygl_font_set (&font5x7_1);
+//    tinygl_text_speed_set (MESSAGE_RATE);
+//    tinygl_text(msg);
+//    tinygl_text_mode_set (TINYGL_TEXT_MODE_SCROLL);
+//    total_updates = strlen(msg) * 5;
+//}
 
 int main (void)
 {
+    // Initialise the program
     system_init();
     tinygl_init (PACER_RATE);
     pacer_init (PACER_RATE);
+    navswitch_init();
 
-    showStartingMessage();
+    // Initial start state of the slider bar
+    uint8_t y1 = 2;
+    uint8_t y2 = 4;
+
+    // Draw initial slider bar
+    tinygl_draw_line (tinygl_point (X_POS, y1), tinygl_point (X_POS, y2), 1);
 
     while (1)
     {
         pacer_wait();
-
         tinygl_update();
+        navswitch_update();
+
+        if (navswitch_push_event_p (NAVSWITCH_SOUTH)) { // South is left when green light is top right of the board
+            if (y2 < 6) { // Check slider bar doesn't go out of bounds
+                tinygl_draw_line(tinygl_point(X_POS, y1), tinygl_point(X_POS, y2), 0); // Turn off old state
+                y1++;
+                y2++;
+                tinygl_draw_line(tinygl_point(X_POS, y1), tinygl_point(X_POS, y2), 1); // Turn on new state
+            }
+        }
+
+        if (navswitch_push_event_p (NAVSWITCH_NORTH)) {
+            if (y1 > 0) { // Check slider bar doesn't go out of bounds
+                tinygl_draw_line(tinygl_point(X_POS, y1), tinygl_point(X_POS, y2), 0); // Turn off old state
+                y1--;
+                y2--;
+                tinygl_draw_line(tinygl_point(X_POS, y1), tinygl_point(X_POS, y2), 1); // Turn on new state
+            }
+        }
 
     }
     return 0;
