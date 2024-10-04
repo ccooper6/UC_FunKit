af6fb13eb5622ab702b6532f6b10058b242c61fb
cco139 <cco139@uclive.ac.nz>
2024-10-04 17:21:02 +1300

    Starting to get ball logic working

--- game.c
+++ game.c
@@ -3,6 +3,7 @@
 #include "tinygl.h"
 #include "../fonts/font5x7_1.h"
 #include "navswitch.h"
+#include "usart1.h"
 
 #define PACER_RATE 500
 #define MESSAGE_RATE 10
@@ -28,18 +29,29 @@ int main (void)
     pacer_init (PACER_RATE);
     navswitch_init();
 
+
     // Initial start state of the slider bar
     uint8_t y1 = 2;
     uint8_t y2 = 4;
 
+    // Initial start state of the ball
+    uint8_t ball_x1 = 0;
+    uint8_t ball_y1 = 3;
+
     // Draw initial slider bar
     tinygl_draw_line (tinygl_point (X_POS, y1), tinygl_point (X_POS, y2), 1);
+
+    tinygl_draw_point(tinygl_point (ball_x1, ball_y1), 1);
+
+    uint16_t ball_tick = 0;
 
     while (1)
     {
         pacer_wait();
         tinygl_update();
         navswitch_update();
+        ball_tick++;
+
 
         if (navswitch_push_event_p (NAVSWITCH_SOUTH)) { // South is left when green light is top right of the board
             if (y2 < 6) { // Check slider bar doesn't go out of bounds
@@ -58,6 +70,39 @@ int main (void)
                 tinygl_draw_line(tinygl_point(X_POS, y1), tinygl_point(X_POS, y2), 1); // Turn on new state
             }
         }
+        if (ball_tick >= 100) {
+            ball_tick = 0;
+            if (ball_x1 == 4 && ((ball_y1 < y1 && ball_y1 < y2) || (ball_y1 > y1 && ball_y1 > y2))){
+                tinygl_draw_point(tinygl_point (ball_x1, ball_y1), 0);
+                ball_x1 = 0;
+                tinygl_draw_point(tinygl_point (ball_x1, ball_y1), 1);
+            } else if (ball_x1 == 3 && !((ball_y1 < y1 && ball_y1 < y2) || (ball_y1 > y1 && ball_y1 > y2))){
+                if (ball_y1 == y1) {
+                    //Go left
+                    tinygl_draw_point(tinygl_point (ball_x1, ball_y1), 0);
+                    tinygl_draw_point(tinygl_point (ball_x1--, ball_y1--), 1);
+
+                } else if (ball_y1 == y2) {
+                    continue;
+                } else {
+                    continue;
+                }
+
+                tinygl_draw_point(tinygl_point (ball_x1, ball_y1), 0);
+                ball_x1 = 0;
+                tinygl_draw_point(tinygl_point (ball_x1, ball_y1), 1);
+            } else {
+                tinygl_draw_point(tinygl_point (ball_x1, ball_y1), 0);
+                ball_x1++;
+                tinygl_draw_point(tinygl_point (ball_x1, ball_y1), 1);
+            }
+
+            // if barrier is there then not allowed past x=3, if barrier not there it can go to x=4. After both cases, reset to x = 0
+        }
+
+
+
+
 
     }
     return 0;
